94cf87610e873a984540d50e9d976433db7fb567
chenhaoyu <chenhaoyu@bjsasc.com>
2022-04-08 09:00:48 +0800

    细化执行类型

--- src/main/java/com/lagou/pojo/ExcuteType.java
+++ src/main/java/com/lagou/pojo/ExcuteType.java
@@ -2,5 +2,16 @@ package com.lagou.pojo;
 
 public enum ExcuteType {
 
-	SELECT,DELETE,UPDATE,INSERT
+	SELECT(0,"查询"),
+	DELETE(1,"删除"),
+	UPDATE(2,"修改"),
+	INSERT(3,"新建");
+
+	private int code;
+	private String desc;
+
+	ExcuteType(int code, String desc) {
+		this.code = code;
+		this.desc = desc;
+	}
 }
